c94d78a2537c9c8de64c7193263d48f6c2523f3f
CasterWx <1325200471@qq.com>
2018-12-30 21:15:34 +0800

    source

--- src/bitmaps/DosInterface.java
+++ src/bitmaps/DosInterface.java
@@ -14,23 +14,29 @@ public class DosInterface {
         Qrcode qrcode = new Qrcode();
         qrcode.setQrcodeErrorCorrect('M');
         qrcode.setQrcodeEncodeMode('B');
-        qrcode.setQrcodeVersion(7);
+        qrcode.setQrcodeVersion(3);
         BufferedImage writeImg = ImageIO.read(new File("1.jpg"));
         BufferedImage bf = new BufferedImage(writeImg.getWidth(),writeImg.getHeight(),BufferedImage.TYPE_INT_RGB);
         Graphics2D gs = bf.createGraphics() ;
         gs.setBackground(Color.white);
         gs.clearRect(0,0,bf.getWidth(),bf.getHeight());
         gs.drawImage(writeImg,0,0,bf.getWidth(),bf.getHeight(),null);
-        gs.setColor(Color.black);
 
         byte []contentByte = content.getBytes("utf-8");
         boolean [][]cidesOut = qrcode.calQrcode(contentByte) ;
-        int mini = bf.getHeight()/cidesOut.length ;
-        int begin = (bf.getHeight() - mini*cidesOut.length )/2;
+        int qrLength = writeImg.getWidth()-2*writeImg.getWidth()/10 ;
+        int mini = qrLength/cidesOut.length ;
+        int begin = (writeImg.getWidth() - mini*cidesOut.length)/2 ;
         for(int i=0;i<cidesOut.length;i++){
             for(int j=0;j<cidesOut.length;j++){
                 if(cidesOut[j][i]){
+                    gs.setColor(Color.BLACK);
+                    gs.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,1.0f));
                     gs.fillRect(j*mini+begin,i*mini+begin,mini,mini );
+                }else{
+//                    gs.setColor(Color.white);
+//                    gs.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,0.8f));
+//                    gs.fillRect(j*mini+begin,i*mini+begin,mini,mini );
                 }
             }
         }
